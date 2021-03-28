697809b8f160096a8a21f4c9862878b2f68e9b62
Akhileh01mishra <81490027+Akhileh01mishra@users.noreply.github.com>
2021-03-28 20:09:23 +0530

    Create task4.java

---
+++ task4.java
@@ -0,0 +1,19 @@
+package switchcase;
+public class switchcasw {
+  public static void main(String() args) {
+    int choice;
+    system.out.println("pick one : 1, hi\t2 , hey\t3 , Hello\t);
+    Scanner s .= new Scanner(System.in)
+    choice=a.nextInt() ;
+    switch(choice)
+    { 
+       case 1 : system.out.println("you said hi");
+               break;
+       case 2 : system.out.println("you said hi");
+               break;
+       case 3 : system.out.println("you said hi");
+               break;
+               default : System.out.println("invalid choice");
+    }
+ }   
+)   
