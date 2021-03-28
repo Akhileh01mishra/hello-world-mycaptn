f143054a33da50a7d670d24b083c498ab7bdaac3
Akhileh01mishra <81490027+Akhileh01mishra@users.noreply.github.com>
2021-03-28 21:05:37 +0530

    Create task6.java

---
+++ task6.java
@@ -0,0 +1,13 @@
+class test{
+public static void main(string[] args){
+int a=0,b=1,c;
+system.out.print(a.out.print(a+""+b);
+for(int i=1; i<=10;i++){
+c=a+b;
+system.out.print(c);
+a=b;
+b=c;
+}
+}
+}                
+  
