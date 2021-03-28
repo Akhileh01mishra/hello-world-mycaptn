316ff05f03063e552d79e5d3d7cf5cb7f91405d9
Akhileh01mishra <81490027+Akhileh01mishra@users.noreply.github.com>
2021-03-28 20:31:01 +0530

    Create task 5.java

---
+++ task 5.java
@@ -0,0 +1,20 @@
+class Test{
+public static void main(String[] args ){
+scanner sc = new scanner(System.in);
+system.out.println("enter a value");
+int num = sc.nextInt();
+boolean isPrime = true;
+for( int i = 2;i<num;i++){
+num = 29
+if(num%i==0){
+isPrime = false;
+break;
+}
+}
+if(isPrime){
+system.out.println(num+"is a prime number");
+}else{
+ system.out.println(num+"is not a prime number");
+}  
+  
+  
