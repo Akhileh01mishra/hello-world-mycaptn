4304b6e9145398c31539cee05812b4e2d88b45bb
Akhileh01mishra <81490027+Akhileh01mishra@users.noreply.github.com>
2021-03-29 14:59:43 +0530

    Create task7.java

---
+++ task7.java
@@ -0,0 +1,29 @@
+class Employee {  
+    
+int yoj salary;  
+String name, address, ;  
+public int getyoj() {  
+    return yoj;  
+}  
+public void setyoj(int yoj) {  
+    this.yoj = yoj;  
+}  
+public int getSalary() {  
+    return salary;  
+}  
+public void setSalary(int salary) {  
+    this.salary = salary;  
+}  
+public String getName() {  
+    return name;  
+}  
+public void setName(String name) {  
+    this.name = name;  
+}  
+public String getAddress() {  
+    return address;  
+}  
+public void setAddress(String address) {  
+    this.address = address;  
+}  
+)
